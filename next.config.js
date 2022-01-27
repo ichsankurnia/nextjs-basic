6bf7b06fda4770f4ee7806d7adcb63649fc1590d
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-27 20:41:19 +0700

    allow domain for image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'picsum.photos', 'wallpaperplay.com', 'i.pinimg.com', 'wallpaperaccess.com', '66.media.tumblr.com', 'wallpapercave.com', 'www.elsetge.cat', 'www.wallpaperback.net',
+      'getwallpapers.com', 'www.ecopetit.cat', 'cdn.wallpapersafari.com', 'www.jakpost.travel', '3.bp.blogspot.com', 'thewallpaper.co'
+  ],
+  },
 }
